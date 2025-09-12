e4339ee4dd8669fcd41fb15bd71f1ced9994b35f
ibrahima333 <keitasoryibrahima123@gmail.com>
2025-09-12 15:24:58 +0000

    jour2

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -6,17 +6,9 @@ from listings.models import Band
 
 def hello(request):
     bands = Band.objects.all()
-    print(bands)
-    
-    
-    return HttpResponse(f"""<h1>Hello Django!</h1>
-                        <p> mes paragraphe prefere :</p>
-                        <ul> 
-                            <li>{bands[0].name}</li>
-                            <li>{bands[1].name}</li>
-                         </ul>
-                         <p> les titres : {bands[2].title}</p>
-                         <p> les titres : {bands[3].title}</p>""")
+    # return HttpResponse("listings/hello.html")
+    return render(request, 'listings/hello.html',
+                            {'band' : bands })
 
 def about(request):
     return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
